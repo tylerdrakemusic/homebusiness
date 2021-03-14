ef26dc8d2e151265534f30be57df16f1f913b37c
Tyler Drake <tyler.drake@vibranttropical.com>
2021-03-14 15:44:24 -0600

    added input buttons for products

---
+++ javascript.js
@@ -0,0 +1,23 @@
+ $('.plus-btn').on('click', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest('div').find('input');
+    var value = parseInt($input.val());
+ 
+    value++;
+    $input.val(value);
+});
+
+$('.minus-btn').on('click', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest('div').find('input');
+    var value = parseInt($input.val());
+ 
+    if(value!==0){
+        value--;
+    }
+
+  $input.val(value);
+ 
+});
