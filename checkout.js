99dd2dc7009bdf5ca996dfccc0c6dbdfc9d67ae3
Tyler Drake <tyler.drake@vibranttropical.com>
2021-04-17 17:11:07 -0600

    1st iteration order flow

---
+++ checkout.js
@@ -0,0 +1,26 @@
+function printOrderSummary() {
+    console.log(sessionStorage);
+  
+    var para = document.createElement("p");
+    para.className="productsummary";
+  
+    for (let i = 0; i < sessionStorage.length; i++) {
+      var element = JSON.parse(sessionStorage.getItem(sessionStorage.key(i)));
+      if(element.subproduct!=null){
+      var node = document.createTextNode(element.product + ' ' + element.subproduct + '........quantity=' + element.value + '\n');
+      para.appendChild(node);
+      para.appendChild(document.createElement('br'));
+      
+      }
+    }
+    para.appendChild(document.createElement('br'));
+    para.appendChild(document.createElement('br'));
+    var dollarTotal = sessionStorage.getItem('dollarTotal');
+    if(dollarTotal!=null){
+      var node = document.createTextNode('Order Total = $' + dollarTotal);
+      para.appendChild(node);
+    }
+  
+    var element = document.getElementById("div1");
+    element.appendChild(para);
+  }
