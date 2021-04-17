a1f1acedd51c73b251b6bb1f0fea51f90e42e4c2
Tyler Drake <tyler.drake@vibranttropical.com>
2021-04-17 17:10:00 -0600

    1st iteration checkout

--- javascript.js
+++ javascript.js
@@ -3,13 +3,39 @@
     var $this = $(this);
     var $input = $this.closest('div').find('input');
     var value = parseInt($input.val());
-
-    value++;
-    var cartTotal = parseInt(document.getElementById('lblCartCount').textContent);
-    document.getElementById('lblCartCount').textContent = cartTotal+1;
+    var dollars = (parseInt($this.closest('div').find('h4')[0].textContent.replace(/[^0-9\.]/g, '')));
+    var product = $this.closest('div.menuitem').find('h4')[0].textContent;
+    var subproduct = $this.closest('div').find('h4')[0].textContent;
     value = isNaN(value) ? 0 : value;
+    value++;
+
+    var cartTotal = parseInt(sessionStorage.getItem("cartTotal"));
+    if(isNaN(cartTotal)){
+      cartTotal = 0;
+    }
+    cartTotal = cartTotal+1;
+    
+    var dollarTotal = parseInt(sessionStorage.getItem("dollarTotal"));
+    if(isNaN(dollarTotal)){
+      dollarTotal = 0;
+    }
+    dollarTotal = dollarTotal + dollars;
+
+    document.getElementById('lblCartCount').textContent = cartTotal;
+    document.getElementById('lblDollarCount').textContent = '$' + (dollarTotal);
     $input.val(value);
-    document.getElementById('total').innerHTML = value;
+
+    var entry = {
+        "product": product,
+        "subproduct": subproduct,
+        "dollars" : dollars,
+        "value" : value
+    };
+    
+    sessionStorage.setItem("cartTotal", cartTotal);
+    sessionStorage.setItem("dollarTotal", dollarTotal);
+    sessionStorage.setItem(subproduct, JSON.stringify(entry));
+
 });
 
 $('.minus-btn').on('click', function(e) {
@@ -17,15 +43,39 @@ $('.minus-btn').on('click', function(e) {
     var $this = $(this);
     var $input = $this.closest('div').find('input');
     var value = parseInt($input.val());
- 
+    var dollars = (parseInt($this.closest('div').find('h4')[0].textContent.replace(/[^0-9\.]/g, '')));
+    var product = $this.closest('div.menuitem').find('h4')[0].textContent;
+    var subproduct = $this.closest('div').find('h4')[0].textContent;
+    
+    value = isNaN(value) ? 0 : value;
     if(value!==0){
         value--;
-        var cartTotal = parseInt(document.getElementById('lblCartCount').textContent);
-        document.getElementById('lblCartCount').textContent = cartTotal-1;
+        var cartTotal = parseInt(sessionStorage.getItem("cartTotal"));
+        if(isNaN(cartTotal)){
+          cartTotal = 0;
+        }
+        cartTotal = cartTotal-1;
+        var dollarTotal = parseInt(sessionStorage.getItem("dollarTotal"));
+        if(isNaN(dollarTotal)){
+          dollarTotal = 0;
+        }
+        dollarTotal = dollarTotal - dollars;
+        document.getElementById('lblCartCount').textContent = cartTotal;
+        document.getElementById('lblDollarCount').textContent = '$' + (dollarTotal);
     }
-    value = isNaN(value) ? 0 : value;
-
   $input.val(value);
 
-  document.getElementById('total').innerHTML = value;
+  var entry = {
+      "product": product,
+      "subproduct": subproduct,
+      "dollars" : dollars,
+      "value" : value
+  };
+  sessionStorage.setItem("cartTotal", cartTotal);
+  sessionStorage.setItem("dollarTotal", dollarTotal);
+  sessionStorage.setItem(subproduct, JSON.stringify(entry));
 });
+
+window.onload = function () {
+  sessionStorage.clear();
+}
