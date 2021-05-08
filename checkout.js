2253bdac1aa13d4798a7736a7e44b804d1611880
Tyler Drake <tyler.drake@vibranttropical.com>
2021-05-08 14:12:22 -0600

    Shipping + policy + order check

--- checkout.js
+++ checkout.js
@@ -24,3 +24,35 @@ function printOrderSummary() {
     var element = document.getElementById("div1");
     element.appendChild(para);
   }
+
+  window.onload = function checkboxShipping() {
+    var shipCheckBox = document.getElementById("sameaddresscheckbox");
+    shipCheckBox.onclick = function(){
+      checkboxShipping();
+    };
+    var shipdiv = document.getElementById("shipdiv");
+  
+    if (shipCheckBox.checked == false){
+      shipdiv.style.display = "block";
+    } else {
+      shipdiv.style.display = "none";
+    }
+    checkboxPolicy();
+  }
+
+  function checkboxPolicy(){
+    var policyCheckBox = document.getElementById("policycheckbox");
+    var checkoutCheckBox = document.getElementById("checkoutcheckbox");
+    policyCheckBox.onclick = function (){
+      checkboxPolicy();
+    }
+    checkoutCheckBox.onclick = function(){
+      checkboxPolicy();
+    }
+    if(policyCheckBox.checked==true && checkoutCheckBox.checked==true){
+      document.getElementById("checkout").disabled = false;
+    }
+    else{
+      document.getElementById("checkout").disabled = true;
+    }
+  }
