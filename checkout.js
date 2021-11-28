5183b9d85d85bb20f4dac65d3df76845f1fcccad
Tyler Drake <tyler.drake@vibranttropical.com>
2021-11-28 14:09:58 -0700

    Updated connections.  subproduct updated

--- checkout.js
+++ checkout.js
@@ -6,9 +6,9 @@ function printOrderSummary() {
 
   for (let i = 0; i < sessionStorage.length; i++) {
     var element = JSON.parse(sessionStorage.getItem(sessionStorage.key(i)));
-    if (element.subproduct != null) {
-      console.log(element);
-      var node = document.createTextNode(element.productName + ' ' + element.subproduct + '...... quantity=' + element.quantity + '\n');
+    if (element.subProduct != null) {
+      //console.log(element);
+      var node = document.createTextNode(element.productName + ' ' + element.subProduct + '...... quantity=' + element.quantity + '\n');
       para.appendChild(node);
       para.appendChild(document.createElement('br'));
 
@@ -77,12 +77,12 @@ function handleSubmit(event) {
   }
   
   requestBody.products = products.products;
-  console.log(requestBody.products);
+  //console.log(requestBody.products);
   userAction(requestBody);
 }
 
 const userAction = async (requestBody) => {
-  console.log( requestBody.valueOf() );
+  //console.log( requestBody.valueOf() );
   const response = await fetch('http://localhost:8080/order', {
     method: 'POST',
     body: JSON.stringify(requestBody),
