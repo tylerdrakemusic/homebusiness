1aae7e1193c35fc8b7a6d4f5cc92ba3730f09d0d
Abbey Drake <drakeam1189@gmail.com>
2022-01-19 17:18:32 -0700

    added skeleton of carousel to story page

--- javascript.js
+++ javascript.js
@@ -87,3 +87,18 @@ window.onload = function () {
     sessionStorage.clear();
   }
 }
+
+//Code below to make button pause/play - not working
+  $(".carousel").carousel( { interval: 2000 } );
+
+  $("#carouselButton").click(function(){
+      if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+          $(".carousel").carousel("pause");
+          $("#carouselButton").children("i").removeClass("fa-pause");
+          $("#carouselButton").children("i").addClass("fa-play");
+      } else {
+          $(".carousel").carousel("cycle");
+          $("#carouselButton").children("i").removeClass("fa-play");
+          $("#carouselButton").children("i").addClass("fa-pause"); 
+      }
+});
