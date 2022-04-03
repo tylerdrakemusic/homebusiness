2940542c0ce87ed76bf1e3beec54c695c98cb193
Abbey Drake <drakeam1189@gmail.com>
2022-04-03 15:52:04 -0600

    story.js

---
+++ story.js
@@ -0,0 +1,92 @@
+const STORYLIST = [
+    {
+        id: 0,
+        name: "Breeder Setup in Office",
+        image: "/img/story/40BreederSetupInOffice.jpg",
+        description: ""
+    },
+    {
+        id: 1,
+        name: "Carpeted Tank Start",
+        image: "/img/story/CarpetedTankStart.jpg",
+        description: ""
+    },
+    {
+        id: 2,
+        name: "Diamond Hole Saw",
+        image: "/img/story/DiamondHoleSaw.jpg",
+        description: ""
+    },
+    {
+        id: 3,
+        name: "First Two Rows with Lights",
+        image: "/img/story/First2RowsWithLights.jpg",
+        description: ""
+    },
+    {
+        id: 4,
+        name: "First 10g Aquascape with Plants",
+        image: "/img/story/first10gaquascapewithplants.jpg",
+        description: ""
+    },
+    {
+        id: 5,
+        name: "First 10g Aquascape with Plants",
+        image: "/img/story/first10gaquascapewithplants2.jpg",
+        description: ""
+    },
+    {
+        id: 6,
+        name: "First 29g Setup",
+        image: "/img/story/first29gSetup.jpg",
+        description: ""
+    },
+    {
+        id: 7,
+        name: "First Guppy Fry",
+        image: "/img/story/firstGuppyFry.jpg",
+        description: ""
+    },
+    {
+        id: 8,
+        name: "Fish Rack Pieces",
+        image: "/img/story/FishRackPieces.jpg",
+        description: ""
+    },
+    {
+        id: 9,
+        name: "Fish Stand Design",
+        image: "/img/story/FishStandDesign.jpg",
+        description: ""
+    },
+    {
+        id: 10,
+        name: "First 10g Aquascape Hard",
+        image: "/img/story/fisrt10gaquascapehard.jpg",
+        description: ""
+    },
+    {
+        id: 11,
+        name: "Grandpa Working on Fish Rack",
+        image: "/img/story/GrandpaWorkingOnFishRack.jpg",
+        description: ""
+    },
+    {
+        id: 12,
+        name: "Holes Drilled Good Design",
+        image: "/img/story/HolesDrilledGoodDesign.jpg",
+        description: ""
+    },
+    {
+        id: 13,
+        name: "Pleco With Juvenile Cichlids",
+        image: "/img/story/PlecoWithJuvenileCichlids.jpg",
+        description: ""
+    },
+    {
+        id: 14,
+        name: "Side of Fish Rack",
+        image: "/img/story/SideOfFishRack.jpg",
+        description: ""
+    },
+]
