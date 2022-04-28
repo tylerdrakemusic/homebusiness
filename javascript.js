49fafe004d601db11750e55aaadf9b588034b0f5
Abbey Drake <drakeam1189@gmail.com>
2022-04-28 09:39:34 -0600

    jumbotron style

--- javascript.js
+++ javascript.js
@@ -98,16 +98,16 @@ function readConfiguration(url){
 }
 
 //Code below to make button pause/play - not working
-  $(".carousel").carousel( { interval: 2000 } );
+//   $(".carousel").carousel( { interval: 2000 } );
 
-  $("#carouselButton").click(function(){
-      if ($("#carouselButton").children("i").hasClass("fa-pause")) {
-          $(".carousel").carousel("pause");
-          $("#carouselButton").children("i").removeClass("fa-pause");
-          $("#carouselButton").children("i").addClass("fa-play");
-      } else {
-          $(".carousel").carousel("cycle");
-          $("#carouselButton").children("i").removeClass("fa-play");
-          $("#carouselButton").children("i").addClass("fa-pause"); 
-      }
-});
+//   $("#carouselButton").click(function(){
+//       if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+//           $(".carousel").carousel("pause");
+//           $("#carouselButton").children("i").removeClass("fa-pause");
+//           $("#carouselButton").children("i").addClass("fa-play");
+//       } else {
+//           $(".carousel").carousel("cycle");
+//           $("#carouselButton").children("i").removeClass("fa-play");
+//           $("#carouselButton").children("i").addClass("fa-pause"); 
+//       }
+// });
