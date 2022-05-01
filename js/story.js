bae03013e0013e33547e236626b0c9b8c5a57cf1
Abbey Drake <drakeam1189@gmail.com>
2022-05-01 17:28:41 -0600

    footer and reordered carousel

--- js/story.js
+++ js/story.js
@@ -3,14 +3,14 @@ function renderCarousel() {
     const STORYLIST = [
         {
             id: 0,
-            name: "Breeder Setup in Office",
-            image: "/img/story/40BreederSetupInOffice.jpg",
-            description: "Setting up a tank with a plant carpet in office."
+            name: "Fish Stand Design",
+            image: "/img/story/FishStandDesign.jpg",
+            description: "Original design plans for basement aquarium"
         },
         {
             id: 1,
-            name: "Carpeted Tank Start",
-            image: "/img/story/CarpetedTankStart.jpg",
+            name: "Fish Rack Pieces",
+            image: "/img/story/FishRackPieces.jpg",
             description: ""
         },
         {
@@ -21,76 +21,77 @@ function renderCarousel() {
         },
         {
             id: 3,
-            name: "First Two Rows with Lights",
-            image: "/img/story/First2RowsWithLights.jpg",
+            name: "Holes Drilled",
+            image: "/img/story/HolesDrilledGoodDesign.jpg",
             description: ""
         },
         {
             id: 4,
-            name: "First 10g Aquascape with Plants",
-            image: "/img/story/first10gaquascapewithplants.jpg",
-            description: ""
-        },
-        {
-            id: 5,
-            name: "First 10g Aquascape with Plants",
-            image: "/img/story/first10gaquascapewithplants2.jpg",
-            description: ""
-        },
-        {
-            id: 6,
-            name: "First 29g Setup",
-            image: "/img/story/first29gSetup.jpg",
-            description: ""
-        },
-        {
-            id: 7,
-            name: "First Guppy Fry",
-            image: "/img/story/firstGuppyFry.jpg",
-            description: ""
-        },
-        {
-            id: 8,
-            name: "Fish Rack Pieces",
-            image: "/img/story/FishRackPieces.jpg",
-            description: ""
-        },
-        {
-            id: 9,
-            name: "Fish Stand Design",
-            image: "/img/story/FishStandDesign.jpg",
-            description: "Original design plans for basement aquarium"
-        },
-        {
-            id: 10,
-            name: "First 10g Aquascape Hard",
-            image: "/img/story/fisrt10gaquascapehard.jpg",
-            description: ""
-        },
-        {
-            id: 11,
             name: "Grandpa Working on Fish Rack",
             image: "/img/story/GrandpaWorkingOnFishRack.jpg",
             description: "My grandfather at 90 years old, a lifelong engineer and woodworker, helping me build my tank racks."
         },
         {
-            id: 12,
-            name: "Holes Drilled Good Design",
-            image: "/img/story/HolesDrilledGoodDesign.jpg",
-            description: ""
-        },
-        {
-            id: 13,
-            name: "Pleco With Juvenile Cichlids",
-            image: "/img/story/PlecoWithJuvenileCichlids.jpg",
-            description: ""
-        },
-        {
-            id: 14,
+            id: 5,
             name: "Side of Fish Rack",
             image: "/img/story/SideOfFishRack.jpg",
             description: ""
         },
+        {
+            id: 6,
+            name: "First Two Rows with Lights",
+            image: "/img/story/First2RowsWithLights.jpg",
+            description: ""
+        },
+        {
+            id: 7,
+            name: "Breeder Setup in Office",
+            image: "/img/story/40BreederSetupInOffice.jpg",
+            description: "Setting up a tank with a plant carpet in office."
+        },
+        {
+            id: 8,
+            name: "Carpeted Tank Start",
+            image: "/img/story/CarpetedTankStart.jpg",
+            description: ""
+        },
+        {
+            id: 9,
+            name: "First 10 gallon Aquascape",
+            image: "/img/story/fisrt10gaquascapehard.jpg",
+            description: ""
+        },
+        {
+            id: 10,
+            name: "First 10 gallon Aquascape with Plants",
+            image: "/img/story/first10gaquascapewithplants.jpg",
+            description: ""
+        },
+        {
+            id: 11,
+            name: "First 10 gallon Aquascape with Plants",
+            image: "/img/story/first10gaquascapewithplants2.jpg",
+            description: ""
+        },
+        {
+            id: 12,
+            name: "First 29 gallon Setup",
+            image: "/img/story/first29gSetup.jpg",
+            description: ""
+        },
+        {
+            id: 13,
+            name: "First Guppy Fry",
+            image: "/img/story/firstGuppyFry.jpg",
+            description: ""
+        },
+        {
+            id: 14,
+            name: "Pleco With Juvenile Cichlids",
+            image: "/img/story/PlecoWithJuvenileCichlids.jpg",
+            description: ""
+        }
+        
     ]
 
     var orderedList = document.createElement("ol");
